bc4bfeb91c993622893688df62419ff2dec996b5
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2023-11-08 12:45:47 +0200

    Fixed the bug for none of blogs

--- frontend/blog-client/src/components/Blogs.js
+++ frontend/blog-client/src/components/Blogs.js
@@ -7,20 +7,22 @@ function Blogs() {
 
     useEffect(() => {
         axios.get('http://localhost:4000/blogs/all')
-        .then(response => {
-            console.log(response.data.blogs)
-            setBlogs(response.data.blogs)
-        })
-        .catch(err => console.log(err))
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data.blogs)
+                    setBlogs(response.data.blogs)
+                }
+            })
+            .catch(err => console.log(err))
     }, []);
-    
 
-    return(
+
+    return (
         <div>
             <ul>
-                {blogs.map( (blog) => (
+                {blogs.map((blog) => (
                     <li key={blog._id}> {blog.title}  {blog.content} {blog.user.username}</li>
-                ) )}
+                ))}
             </ul>
         </div>
     );
