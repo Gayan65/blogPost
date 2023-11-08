96cb16743422aae213739f6964c74b3317ae2b90
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-11-08 20:08:31 +0200

    Register component updated

--- frontend/blog-client/src/components/Register.js
+++ frontend/blog-client/src/components/Register.js
@@ -11,6 +11,7 @@ function Register() {
     const [password, setPassword] = useState("");
     const [fname, setFname] = useState("");
     const [nickName, setNickName] = useState("");
+    const [apiResponse, setApiResponse] = useState({});
 
     useEffect(() => {
         sessionStorage.clear();
@@ -35,11 +36,14 @@ function Register() {
                 }
                 else {
                     console.log(response.data.message);
+                    setApiResponse(response.data);
                 }
             })
             .catch(error => console.log(error));
         setUsername("");
         setPassword("");
+        setFname("");
+        setNickName("");
     }
 
     return (
@@ -53,22 +57,23 @@ function Register() {
                         <form method="POST" onSubmit={handleSubmit}>
                             <h1 className="h3 mb-3 fw-normal">Please Signup</h1>
                             <div className="form-floating">
-                                <input type="text" className="form-control mb-3 input-custom-css" id="floatingInput" placeholder="name@example.com" onChange={(e) => setUsername(e.target.value)} name="username" required value={username} />
+                                <input type="text" className="form-control mb-3 input-custom-css" placeholder="name@example.com" onChange={(e) => setUsername(e.target.value)} name="username" required value={username} />
                                 <label htmlFor="floatingInput">Email address</label>
                             </div>
                             <div className="form-floating">
-                                <input type="text" className="form-control mb-3 input-custom-css" id="floatingPassword" autoComplete="on" placeholder="Password" onChange={(e) => setFname(e.target.value)} name="fname" required value={fname} />
+                                <input type="text" className="form-control mb-3 input-custom-css" autoComplete="on" placeholder="Password" onChange={(e) => setFname(e.target.value)} name="fname" required value={fname} />
                                 <label htmlFor="floatingPassword">Full Name</label>
                             </div>
                             <div className="form-floating">
-                                <input type="text" className="form-control mb-3 input-custom-css" id="floatingPassword" autoComplete="on" placeholder="Password" onChange={(e) => setNickName(e.target.value)} name="nickName" required value={nickName} />
+                                <input type="text" className="form-control mb-3 input-custom-css" autoComplete="on" placeholder="Password" onChange={(e) => setNickName(e.target.value)} name="nickName" required value={nickName} />
                                 <label htmlFor="floatingPassword">Nick Name</label>
                             </div>
                             <div className="form-floating">
-                                <input type="password" className="form-control mb-3 input-custom-css" id="floatingPassword" autoComplete="on" placeholder="Password" onChange={(e) => setPassword(e.target.value)} name="password" required value={password} />
+                                <input type="password" className="form-control mb-3 input-custom-css" autoComplete="on" placeholder="Password" onChange={(e) => setPassword(e.target.value)} name="password" required value={password} />
                                 <label htmlFor="floatingPassword">Password</label>
                             </div>
                             <button className="btn btn-primary w-100 py-2 login-btn" type="submit">Register</button>
+                            {apiResponse.success === false && <div className="alert alert-danger mt-3" role="alert"> {apiResponse.message} </div>}
                         </form>
                     </main>
                 </div>
