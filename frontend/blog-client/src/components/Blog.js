b1cd94d79cde795aef748b122607089e749674f8
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-11-09 20:55:47 +0200

    Blog component bugs in route fixed

--- frontend/blog-client/src/components/Blog.js
+++ frontend/blog-client/src/components/Blog.js
@@ -1,16 +1,49 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import qs from "qs";
+import NavBar from "./NavBar";
 
-function Blog(props) {
+function Blog() {
     const [editClicked, setEditClicked] = useState(false);
     const [titleValue, setTileValue] = useState("");
     const [contentValue, setContentValue] = useState("");
 
+    const[title, setTitle] = useState("");
+    const[content, setContent] = useState("");
+    const[_id, setId] = useState("");
+
     let { state } = useLocation();
-    const { title, content, _id } = state;
-    //console.log(title, content, _id)
+    
+    //console.log(user.username);
+
+    const [auth, setAuth] = useState(false);
+    let user = sessionStorage.getItem('user');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(user === '' || user === null) {
+            navigate('/login');
+            setAuth(false);
+        }
+        else {
+            setAuth(true);
+            if(state) {
+                const { title, content, _id} = state;
+                setTitle(title);
+                setContent(content);
+                setId(_id);
+            }
+            else {
+                navigate('/home');
+            }
+
+        }
+        // eslint-disable-next-line
+    }, []);
+
+
+    const userObj = JSON.parse(user);
 
     async function handleClickDelete() {
         await axios.delete(`http://localhost:4000/user/blog/${_id}`)
@@ -43,24 +76,27 @@ function Blog(props) {
             })
             .catch(error => console.log(error));
     }
-    return (
-        <div>
-            <h1> From Blog </h1>
-            {editClicked ?
-
-                <form>
-                    <h3>Title : <input type="text" value={titleValue} onChange={(e) => setTileValue(e.target.value)} name="title" /> </h3>
-                    <p>Content : <input type="text" value={contentValue} onChange={(e) => setContentValue(e.target.value)} name="content" /> </p>
-                </form> :
+    if(auth) {
+            return (
                 <div>
-                    <h3>Title : {titleValue.length > 0 ? titleValue : title} </h3>
-                    <p>Content : {contentValue.length > 0 ? contentValue : content} </p>
+                    <NavBar auth={true} user={auth ? userObj.username : null} />
+                    <h1> From Blog </h1>
+                    {editClicked ?
+        
+                        <form>
+                            <h3>Title : <input type="text" value={titleValue} onChange={(e) => setTileValue(e.target.value)} name="title" /> </h3>
+                            <p>Content : <input type="text" value={contentValue} onChange={(e) => setContentValue(e.target.value)} name="content" /> </p>
+                        </form> :
+                        <div>
+                            <h3>Title : {titleValue.length > 0 ? titleValue : title} </h3>
+                            <p>Content : {contentValue.length > 0 ? contentValue : content} </p>
+                        </div>
+                    }
+                    {editClicked ? <button onClick={handleClickOk}>Ok</button> : <button onClick={handleClickEdit}>Edit</button>}
+                    <button onClick={handleClickDelete} >Delete</button>
                 </div>
-            }
-            {editClicked ? <button onClick={handleClickOk}>Ok</button> : <button onClick={handleClickEdit}>Edit</button>}
-            <button onClick={handleClickDelete} >Delete</button>
-        </div>
-    );
+            );
+        }
 }
 
 export default Blog;
