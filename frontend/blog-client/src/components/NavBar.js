3ad7fe4deb5f78babe9112369b0903781569299c
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-11-13 08:22:00 +0200

    css framework changed

--- frontend/blog-client/src/components/NavBar.js
+++ frontend/blog-client/src/components/NavBar.js
@@ -1,54 +1,37 @@
 import React from "react";
+import navlog from "../images/navLogo.png"
 import { Link } from "react-router-dom";
 
 
 
 function NavBar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-custom">
-            <div className="container">
-
-                <a className="navbar-brand nav-link-custom" href="/home"> Bloggi  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-chat-left-text-fill" viewBox="0 0 25 25">
-                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z" />
-                </svg> </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active nav-link-custom-other" aria-current="page" href="/about_us">About us</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active nav-link-custom-other" aria-current="page" href="/products">Products</a>
-                        </li>
-                    </ul>
-                    {props.auth ? <form className="d-flex" role="search">     <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item dropdown">
-                                <button className="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                    {props.user}
-                                </button>
-                                <ul className="dropdown-menu dropdown-menu-dark">
-                                    <li> <Link className="dropdown-item" to={'/user/blogs'}> My blogs </Link> </li>
-                                    <li> <Link className="dropdown-item" to={'/user/blogs'}> Profile </Link></li>
-                                    <li> <Link className="dropdown-item" to={'/user/create_blog'}> Create blog </Link> </li>
-                                    <li> <Link className="dropdown-item" to={'/login'}> Logout </Link> </li>
-
-                                </ul>
-                            </li>
-                        </ul>
-                    </div> </form> : <form className="d-flex" role="search">
-                        <Link className="btn btn-outline-success navbar-btn" to={'/login'}>Sign in</Link>
-                        <Link className="btn btn-outline-success navbar-btn" to={'/register'}>Register</Link>
-                    </form>}
-
-
-
-
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" >
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/home">
+                    <img src={navlog} alt="Logo" width="30" height="25" className="d-inline-block align-text-top mx-2" />
+                    Bloggy
+                </a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link" to={'/about_us'}>About Us</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to={'/products'}>Products</Link>
+                    </li>
+                </ul>
+                <div className="d-flex" role="search">
+                    <Link className="btn btn-secondary mx-3" to={'/login'}>Sign in</Link>
+                    <Link className="btn btn-outline-secondary" to={'/register'}>Register</Link>
                 </div>
             </div>
+            </div>
+
+
+
         </nav>
+
     );
 }
 
