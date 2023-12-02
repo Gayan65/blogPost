388424b11d3b5f51afeeee34f0d3fb69baa57d4f
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-12-02 16:37:00 +0200

    Nav bar competeble with products and about us

--- frontend/blog-client/src/components/AboutUs.js
+++ frontend/blog-client/src/components/AboutUs.js
@@ -2,11 +2,22 @@ import React from "react";
 import NavBar from "./NavBar";
 
 function AboutUs() {
+  let user = sessionStorage.getItem("user");
+  const userObj = JSON.parse(user);
+  if (userObj) {
     return (
-        <div>
-            <NavBar auth={false} />
-            <h1>About us</h1>
-        </div>
+      <div>
+        <NavBar auth={true} user={userObj.username} />
+        <h1>About Us</h1>
+      </div>
     );
+  } else {
+    return (
+      <div>
+        <NavBar auth={false} />
+        <h1>About Us</h1>
+      </div>
+    );
+  }
 }
 export default AboutUs;
--- frontend/blog-client/src/components/Products.js
+++ frontend/blog-client/src/components/Products.js
@@ -2,11 +2,22 @@ import React from "react";
 import NavBar from "./NavBar";
 
 function Products() {
+  let user = sessionStorage.getItem("user");
+  const userObj = JSON.parse(user);
+  if (userObj) {
     return (
-        <div>
-            <NavBar auth={false} />
-            <h1>Products</h1>
-        </div>
+      <div>
+        <NavBar auth={true} user={userObj.username} />
+        <h1>Products</h1>
+      </div>
     );
+  } else {
+    return (
+      <div>
+        <NavBar auth={false} />
+        <h1>Products</h1>
+      </div>
+    );
+  }
 }
 export default Products;
