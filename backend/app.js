034c75409451912a8a896f538f63c5de4896e45d
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-12-05 22:44:07 +0200

    Cluster cloud with mongodb

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT;
+const db_url = process.env.DBURL;
 
 app.use(cors());
 app.use(user_route);
@@ -18,9 +19,7 @@ app.use(comment_router);
 app.use(express.static("public"));
 //app.use(bodyParser.json());
 
-await mongoose
-  .connect("mongodb://127.0.0.1:27017/blogpost_db")
-  .then(() => console.log("DB Connected!"));
+await mongoose.connect(db_url).then(() => console.log("DB Connected!"));
 
 app.listen(port, () => {
   console.log(`Server running port number ${port}`);
