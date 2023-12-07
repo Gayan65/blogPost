479912d9524ac1c469cb1f05ed9553109c70bb66
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-12-07 13:49:34 +0200

    Uniqe add to user scheama in back end

--- backend/schemas/User.js
+++ backend/schemas/User.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   password: String,
   fname: String,
   createDate: Date,
